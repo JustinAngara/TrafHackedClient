115c91bd2f9552324b18e1a24c43074710e3bdb3
Justin Angara <justin.angara@gmail.com>
2026-01-04 23:28:58 -0600

    added slightly more jhits

--- src/client/java/com/traf/hacks/AutoHeal.java
+++ src/client/java/com/traf/hacks/AutoHeal.java
@@ -1,9 +1,29 @@
 package com.traf.hacks;
 
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.item.alchemy.Potions;
 
+/**
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 15 | item.minecraft.splash_potion x1
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 16 | item.minecraft.splash_potion x1
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 17 | item.minecraft.splash_potion x1
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 18 | item.minecraft.splash_potion x1
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 19 | item.minecraft.splash_potion x1
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 20 | item.minecraft.splash_potion x1
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 21 | item.minecraft.splash_potion x1
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 22 | item.minecraft.splash_potion x1
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 23 | item.minecraft.splash_potion x1
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 24 | item.minecraft.splash_potion x1
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 0 | item.minecraft.cooked_beef x63
+ * [23:22:43] [Render thread/INFO] (Minecraft) [STDOUT]: Slot 9 | item.minecraft.splash_potion x1
+ *
+ * */
 public class AutoHeal extends Hack {
 
     public AutoHeal(String s) {
@@ -17,9 +37,12 @@ public class AutoHeal extends Hack {
 
         // get inventory to see where the pot is
         Inventory inv = lp.getInventory();
+        ItemStack stack;
+        PotionContents contents;
+
 
         for (int slot = 0; slot < inv.getContainerSize(); slot++) {
-            ItemStack stack = inv.getItem(slot);
+            stack = inv.getItem(slot);
             if (stack.isEmpty()) continue;
 
             System.out.println(
@@ -27,7 +50,16 @@ public class AutoHeal extends Hack {
                             " | " + stack.getItem().getDescriptionId() +
                             " x" + stack.getCount()
             );
+
+            contents = stack.get(DataComponents.POTION_CONTENTS);
+            if (stack.is(Items.SPLASH_POTION) && contents != null) {
+                if (contents.potion().isPresent() && contents.potion().get() == Potions.HEALING) {
+                    System.out.println("this is a splash potion of healing");
+
+                }
+            }
         }
+
         return false;
     }
 }
